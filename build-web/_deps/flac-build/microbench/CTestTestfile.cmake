ac33d9ffe862b7f15230c4d95ce2eb34a0eabb6a
dan52rusik <baryshevdanila@gmail.com>
2026-04-12 12:53:20 +0300

    фикс нинзя буилд

--- build-web/_deps/flac-build/microbench/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/MyGames/FirstRugieLikeDankaise/build-web/_deps/flac-src/microbench
-# Build directory: C:/MyGames/FirstRugieLikeDankaise/build-web/_deps/flac-build/microbench
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build-web/web/index.js
+++ build-web/web/index.js
@@ -71,7 +71,7 @@ var ENVIRONMENT_IS_SHELL = !ENVIRONMENT_IS_WEB && !ENVIRONMENT_IS_NODE && !ENVIR
 
 // --pre-jses are emitted after the Module integration code, so that they can
 // refer to Module (if they choose; they can also define Module)
-// include: C:\Users\Dankarus\AppData\Local\Temp\tmpaxp0crm1.js
+// include: C:\msys64\tmp\tmpiwxrk5v9.js
 
   if (!Module['expectedDataFileDownloads']) Module['expectedDataFileDownloads'] = 0;
   Module['expectedDataFileDownloads']++;
@@ -212,21 +212,21 @@ Module['FS_createPath']("/assets/textures", "ui", true, true);
 
   })();
 
-// end include: C:\Users\Dankarus\AppData\Local\Temp\tmpaxp0crm1.js
-// include: C:\Users\Dankarus\AppData\Local\Temp\tmpvnqq622g.js
+// end include: C:\msys64\tmp\tmpiwxrk5v9.js
+// include: C:\msys64\tmp\tmpvnq3miuq.js
 
     // All the pre-js content up to here must remain later on, we need to run
     // it.
     if ((typeof ENVIRONMENT_IS_WASM_WORKER != 'undefined' && ENVIRONMENT_IS_WASM_WORKER) || (typeof ENVIRONMENT_IS_PTHREAD != 'undefined' && ENVIRONMENT_IS_PTHREAD) || (typeof ENVIRONMENT_IS_AUDIO_WORKLET != 'undefined' && ENVIRONMENT_IS_AUDIO_WORKLET)) Module['preRun'] = [];
     var necessaryPreJSTasks = Module['preRun'].slice();
-  // end include: C:\Users\Dankarus\AppData\Local\Temp\tmpvnqq622g.js
-// include: C:\Users\Dankarus\AppData\Local\Temp\tmpv45pub8o.js
+  // end include: C:\msys64\tmp\tmpvnq3miuq.js
+// include: C:\msys64\tmp\tmpmfvzb8au.js
 
     if (!Module['preRun']) throw 'Module.preRun should exist because file support used it; did a pre-js delete it?';
     necessaryPreJSTasks.forEach((task) => {
       if (Module['preRun'].indexOf(task) < 0) throw 'All preRun tasks that exist before user pre-js code should remain after; did you replace Module or modify Module.preRun?';
     });
-  // end include: C:\Users\Dankarus\AppData\Local\Temp\tmpv45pub8o.js
+  // end include: C:\msys64\tmp\tmpmfvzb8au.js
 
 
 var arguments_ = [];
